2a04e4c2867f6ce4cc3eedead5444556a1e66655
crispto <2273713147@qq.com>
2023-12-10 20:08:17 +0800

    add opencv gui

---
+++ opencv_gui.sh
@@ -0,0 +1,5 @@
+# 在容器中运行 GUI 程序，通过共享主机的 X11 socket 显示出来
+# xhost + 
+docker run --rm -ti -d --net=host --ipc=host \
+   -e DISPLAY=$DISPLAY \
+   -v /tmp/.X11-unix:/tmp/.X11-unix mnieto/opencv_gui bash
